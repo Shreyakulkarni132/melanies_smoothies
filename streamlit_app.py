34c5de76094cd8d7f7d926054ad80ae3ad7a0704
Shreya Kulkarni <shreya.kulkarni132@gmail.com>
2026-02-11 15:14:33 +0530

    Fetch and display nutrition information for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,11 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-         ingredients_string += fruit_chosen + ' '
+      ingredients_string += fruit_chosen + ' '
+      st.subheader(fruit_chosen + 'Nutrition Information')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+      sf_df = st.dataframe(data=smoothiefroot_response.json() , use_container_width=True)
+        
 
     st.write(ingredients_string)
 
@@ -44,6 +48,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 #new section
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json() , use_container_width=True)
+
